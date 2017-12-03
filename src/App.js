01fddd162f875ac0ebb8d23c38faa0e354c6e9d4
Christiane Deneser <christiane.deneser@publicispixelpark.de>
2017-12-03 19:12:14 +0100

    Update state of controlled input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    input: 'Hello'
+  };
+
+  // arrow function allows us to use 'this' to refer to the component itself
+  updateInput = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  };
 
   submit = () => {
     console.log(this.text.value);
@@ -18,8 +28,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>{this.state.input}</h3>
         {/*ref: assign a property to a DOM elem. of your comp., using 'this.[whatevername]' */}
         <input type="text" ref={(input) => this.text = input}/>
+        {/* we want the value of the input to be the state of the input */}
+        <input type="text" onChange={this.updateInput} value={this.state.input}/>
         <button onClick={this.submit}>Show / Hide</button>
       </div>
     );
